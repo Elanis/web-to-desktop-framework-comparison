4276e8cc4240debbad47bddd8865628a5c158b12
Elanis <elanis@hotmail.com>
2021-04-07 12:33:20 +0200

    01-empty-app / 02-empty-app-headless - NeutralinoJS benchmark

---
+++ benchmark/02-empty-app-headless/neutralinojs/resources/js/main.js
@@ -0,0 +1,19 @@
+
+window.myApp = {
+    showInfo: () => {
+        document.getElementById('info').innerHTML = NL_APPID + " is running on port " +
+                    NL_PORT + " inside " + NL_OS + ".<br/><br/>" + "<span>v" + NL_VERSION + "</span>";
+    },
+    openDocs: () => {
+        Neutralino.app.open({
+            url: "https://neutralino.js.org/docs"
+        });
+    }
+};
+
+
+Neutralino.init({
+    onSuccess: () => {
+        window.myApp.showInfo();
+    }
+});
