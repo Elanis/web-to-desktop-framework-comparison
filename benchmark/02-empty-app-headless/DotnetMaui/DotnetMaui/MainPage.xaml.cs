36af51861016522e132331aff7ca1c02b253b4bb
Elanis <elanis@hotmail.com>
2021-09-12 11:49:35 +0200

    Added DotnetMaui headless example
    + Fixed Example #2 README format
    
    Former-commit-id: 7a72fe0edb3d6e098af8391cf862f6537be81195

---
+++ benchmark/02-empty-app-headless/DotnetMaui/DotnetMaui/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace DotnetMaui
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			NavigationPage.SetHasNavigationBar(this, false);
+		}
+
+		int count = 0;
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+			CounterLabel.Text = $"Current count: {count}";
+		}
+	}
+}
---
+++ benchmark/02-empty-app-headless/DotnetMaui/DotnetMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace DotnetMaui
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
