d8ca76b187c65ebad4072b6551fb7e66be1b6763
Elanis <elanis@hotmail.com>
2022-01-23 16:18:37 +0100

    Pre-process runner: npm ci to install dependancies

---
+++ runner/preprocess.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+for app in "$(pwd)/../benchmark"/*/; do
+	for lib in "$app"/*/; do
+		echo -e "Pre-processing $lib"
+		cd "$lib"
+
+		if [ -f "$lib/package.json" ] | [ -f "$lib/package-lock.json" ]; then
+			echo "Detected npm files, installing ..."
+			npm ci
+		fi
+	done
+done
