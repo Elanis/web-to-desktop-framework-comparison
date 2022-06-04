4cafe6d14f6f3cc505041bc7651ae7564b00a4da
Elanis <elanis@hotmail.com>
2022-06-04 12:47:08 +0200

    Install NuGet/Flutter dependencies

--- runner/preprocess.sh
+++ runner/preprocess.sh
@@ -7,8 +7,18 @@ for app in "$(dirname $PWD/$0)/../benchmark"/*/; do
 		cd "$lib"
 
 		if [ -f "$lib/package.json" ] | [ -f "$lib/package-lock.json" ]; then
-			echo "Detected npm files, installing ..."
+			echo "Detected npm files, installing dependencies ..."
 			npm ci
+		fi
+
+		if [ -f "$lib/*.sln" ]; then
+			echo "Detected dotnet files, installing dependencies ..."
+			dotnet restore
+		fi
+
+		if [ -f "$lib/pubspec.yaml" ]; then
+			echo "Detected flutter files, installing dependencies ..."
+			flutter pub get
 		fi
 	done
 done
