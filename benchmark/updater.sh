5cb7702b54bb86437a7d6c80a93085d23e75443b
Elanis <elanis@hotmail.com>
2023-01-30 18:29:15 +0100

    Updater.sh

---
+++ benchmark/updater.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+npm i -g npm-check
+
+function updateDeps() {
+	echo "$PWD"
+
+	if [ -f "package.json" ] | [ -f "package-lock.json" ]; then
+		echo "Detected npm files, checking dependencies ..."
+		npm ci
+		npx npm-check -y
+		npm audit
+		npm audit fix
+		git add package.json package-lock.json yarn.lock
+		git commit -m "[$benchmark] [$framework] Update npm dependencies"
+	fi
+
+	if [ -f "Cargo.toml" ] | [ -f "Cargo.lock" ]; then
+		echo "Detected cargo files, checking dependencies ..."
+		cargo update
+		git add Cargo.toml Cargo.lock
+		git commit -m "[$benchmark] [$framework] Update tauri dependencies"
+	fi
+
+	git clean -xdf
+}
+
+for benchmark in */; do
+	(
+		cd "$benchmark"
+		echo "Scanning $benchmark subdirectories ..."
+		for framework in */; do
+			(
+				echo "Updating $benchmark/$framework ..."
+				cd "$framework"
+
+				updateDeps
+
+				ls $PWD/*/ >/dev/null 2>&1 ; 
+
+				if [ $? == 0 ]; then
+					for subFolder in */; do
+						(
+							cd "$subFolder"
+							updateDeps
+						)
+					done
+				fi
+			)
+		done
+	)
+done
+
+git clean -xdf
