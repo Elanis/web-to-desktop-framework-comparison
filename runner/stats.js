6c09a696a233fdd5342dd7848db4197fc224a660
Elanis <elanis@hotmail.com>
2024-05-05 14:29:42 +0200

    Refacto stats.js

--- runner/stats.js
+++ runner/stats.js
@@ -3,25 +3,24 @@ import fetch from 'node-fetch';
 
 import { libraries } from './config.js';
 
-let data = {};
+const data = {};
 
-(async() => {
-	for(const libraryName in libraries) {
-		const repo = libraries[libraryName].url.replace('https://github.com/', '');
-		const res = await fetch('https://api.github.com/repos/' + repo);
-		const json = await res.json();
+for(const libraryName in libraries) {
+	const repo = libraries[libraryName].url.replace('https://github.com/', '');
+	const res = await fetch('https://api.github.com/repos/' + repo);
+	const json = await res.json();
 
-		const updateDate = new Date(json.pushed_at);
+	const updateDate = new Date(json.pushed_at);
 
-		data[libraryName] = {
-			stars: json.stargazers_count,
-			forks: json.forks_count,
-			lastUpdate: (updateDate.getMonth() + 1) + '/' + updateDate.getFullYear()
-		};
+	data[libraryName] = {
+		stars: json.stargazers_count,
+		forks: json.forks_count,
+		lastUpdate: (updateDate.getMonth() + 1) + '/' + updateDate.getFullYear()
+	};
 
-		console.log(libraryName);
-		console.log(data[libraryName]);
-	}
+	console.log(libraryName);
+	console.log(data[libraryName]);
+}
 
-	fs.writeFileSync('stats.json', JSON.stringify(data));
-})();
+fs.writeFileSync('stats.json', JSON.stringify(data));
+
