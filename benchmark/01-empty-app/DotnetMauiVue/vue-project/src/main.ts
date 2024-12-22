24d18387715d29506e05ee4e953e63c71b60e80b
Elanis <elanis@hotmail.com>
2024-12-22 10:51:12 +0100

    [01-empty-app] [DotnetMAUIVue] Normalize vue example to follow the other ones

--- benchmark/01-empty-app/DotnetMauiVue/vue-project/src/main.ts
+++ benchmark/01-empty-app/DotnetMauiVue/vue-project/src/main.ts
@@ -4,8 +4,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 const app = createApp(App)
 
 console.log("loading vue!")
