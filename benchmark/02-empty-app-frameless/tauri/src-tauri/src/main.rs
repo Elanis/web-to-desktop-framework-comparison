f10f7493f1cfd6e6aba7bc12dc6c742efc2c50ca
Elanis <elanis@hotmail.com>
2024-12-23 08:54:41 +0100

    Fix tauri logging at startup

--- benchmark/02-empty-app-frameless/tauri/src-tauri/src/main.rs
+++ benchmark/02-empty-app-frameless/tauri/src-tauri/src/main.rs
@@ -4,17 +4,11 @@
 )]
 
 use tauri::Listener;
-use tauri::Manager;
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {    
-      // `main` here is the window label; it is defined on the window creation or under `tauri.conf.json`
-      // the default value is `main`. note that it must be unique
-      let main_window = app.get_webview_window("main").unwrap();
-
-      // listen to the `event-name` (emitted on the `main` window)
-      main_window.listen("load", move |_event| {
+      app.listen("load", |_event| {
         println!("App started and loaded !");
       });
       Ok(())
