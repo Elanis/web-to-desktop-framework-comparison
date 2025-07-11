1a23959298109236a0b8c848b782d71b61efe60f
Elanis <elanis@hotmail.com>
2025-07-11 10:16:40 +0200

    Fix nw.js build

--- benchmark/02-empty-app-frameless/nw.js/build.js
+++ benchmark/02-empty-app-frameless/nw.js/build.js
@@ -2,21 +2,22 @@ import os from 'node:os';
 import nwbuild from 'nw-builder';
 
 let archs = [];
-if(os.platform() === 'win32') {
+if (os.platform() === 'win32') {
 	archs = ['ia32', 'x64'];
-} else if(os.platform() === 'darwin') {
+} else if (os.platform() === 'darwin') {
 	archs = ['x64', 'arm64'];
-} else if(os.platform() === 'linux') {
+} else if (os.platform() === 'linux') {
 	archs = ['ia32', 'x64'];
 } else {
 	throw new Error('Unsupported platform !');
 }
 
-for(const arch of archs) {
+for (const arch of archs) {
 	console.log(`Building for ${arch} ...`);
 	await nwbuild({
 		mode: 'build',
 		version: 'stable',
+		manifestUrl: 'https://nwjs.io/versions.json',
 		flavor: 'normal',
 		outDir: `./dist/02-empty-app-frameless-${os.platform()}-${arch}/`,
 		srcDir: './app',
